9d11282382579ff13db482d7bbd260ae862f9436
Christophe Bourelly <bourelly.christophe@gmail.com>
2018-05-06 15:57:34 +0200

    Update scripts to use UTF8MB4 instead of UTF8 (emojii ready)

--- base/EVEMyAdmin.sql
+++ base/EVEMyAdmin.sql
@@ -3,7 +3,7 @@ CREATE TABLE `oauth2_users` (
   `access_token` varchar(1024) NOT NULL DEFAULT '',
   `refresh_token` varchar(1024) NOT NULL DEFAULT '',
   `expire_time` int(11) NOT NULL DEFAULT '-1',
-  `token_type` varchar(255) NOT NULL DEFAULT 'character',
+  `token_type` varchar(255) NOT NULL DEFAULT 'CHARACTER',
   `id_entity` int(11) NOT NULL,
   `id_forum_user` int(11) NOT NULL DEFAULT '1',
   `entity_name` varchar(255) NOT NULL DEFAULT '',
@@ -11,7 +11,7 @@ CREATE TABLE `oauth2_users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `oauth2_users_UNIQUE` (`id_entity`),
   KEY `fk_oauth_users_phpbb_forum_idx` (`id_forum_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `user_session` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -20,4 +20,4 @@ CREATE TABLE `user_session` (
   `current_page` varchar(255) NOT NULL DEFAULT '/',
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id_UNIQUE` (`id_forum_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- base/diff/2018-04-28_Store_character_name_in_DB.sql
+++ base/diff/2018-04-28_Store_character_name_in_DB.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `evemyadmin`.`oauth2_users`
+ALTER TABLE `oauth2_users`
 	ADD COLUMN `character_name` VARCHAR(255) NOT NULL DEFAULT '' AFTER `id_forum_user`,
 	ADD COLUMN `is_main_character` TINYINT(1) NOT NULL DEFAULT '0' AFTER `character_name`;
 
@@ -9,4 +9,4 @@ CREATE TABLE `user_session` (
 	`current_page` varchar(255) NOT NULL DEFAULT '/',
 	PRIMARY KEY (`id`),
 	UNIQUE KEY `user_id_UNIQUE` (`id_forum_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
